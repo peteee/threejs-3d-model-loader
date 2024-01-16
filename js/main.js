28d87137f47d0f004da4304465ab89ec1075140a
Peter Oberlaender <pete@wayoutofline.net>
2024-01-16 11:17:57 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ function init() {
     container = document.getElementById('container');
 
     camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 2000);
-    camera.position.set(8, 10, 8);
+    camera.position.set(8, 20, 8);
     camera.lookAt(0, 3, 0);
 
     scene = new THREE.Scene();
